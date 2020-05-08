c7bc189e024b150b8b2d8c8189dd8aa149573b7f
Marcin Szewczak <marcin.szewczak@icloud.com>
2020-05-08 10:06:10 +0200

    refactoring, added some coments

--- skrypt.py
+++ skrypt.py
@@ -3,22 +3,19 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs 
 
-def get_pdfs(_url):
+def get_pdfs(_url): #find all <a> on page and put to the list all of them that ends with .pdf
     r = requests.get(_url)
     soup = bs(r.content,'html5lib')
     links = soup.findAll('a')
     pdf_links = ["https://link.springer.com" + link['href'] for link in links if link['href'].endswith('pdf')]
-
-    #print(pdf_link)
  
     return pdf_links
 
 def download_pdf(_url, title, cat):
     file_name = title
     print("Downloading book: {}".format(file_name))
-    r = requests.get(_url[0], stream = True)
+    r = requests.get(_url[0], stream = True) # only first link is link to full version in pdf. Other are just chapters.
     dirr = cat + "/" + title + '.pdf'
-    #print (dirr)
     if os.path.exists(dirr):
         pass
     else:
@@ -41,10 +38,9 @@ if __name__ == '__main__':
             os.mkdir(name_)
 
     for i, _url in enumerate(df['OpenURL']):
-        title = df['Book Title'][i].split('/')[0]
+        title = df['Book Title'][i].split('/')[0] # there was an error, in x.xlsx one of the book titles tahe '/' which crashes line 22(with open(...))
         category = df['English Package Name'][i]
-        download_pdf(get_pdfs(_url),title, category)
         print("{}/{}".format(i+1,n))
-
+        download_pdf(get_pdfs(_url),title, category)
 
 ###############################################
